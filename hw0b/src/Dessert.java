f56559f45b0d051562fa9dcff6b50806af45b7ab
harkjeans <chengqi172@gmail.com>
2025-11-06 12:15:59 +0800

    hw0b finished

---
+++ hw0b/src/Dessert.java
@@ -0,0 +1,9 @@
+package PACKAGE_NAME;/**
+* ClassName: Dessert
+* Package: PACKAGE_NAME
+* Description: 
+* @Author harkjeans
+* @Create 2025/11/6 11:59 
+* @Version 1.0   
+*/public class Dessert {
+}
