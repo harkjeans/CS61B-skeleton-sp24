f78506e5ed1aebfb63143e5819577d4195e45207
harkjeans <chengqi172@gmail.com>
2025-11-12 19:12:39 +0800

    proj1c finished

--- proj1c/src/gh2/GuitarString.java
+++ proj1c/src/gh2/GuitarString.java
@@ -1,6 +1,6 @@
 package gh2;
 
-// TODO: maybe more imports
+import deque.*;
 
 //Note: This file will not compile until you complete the Deque61B implementations
 public class GuitarString {
@@ -11,43 +11,58 @@ public class GuitarString {
     private static final double DECAY = .996; // energy decay factor
 
     /* Buffer for storing sound data. */
-    // TODO: uncomment the following line once you're ready to start this portion
-    // private Deque61B<Double> buffer;
+     private Deque61B<Double> buffer;
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
-        // TODO: Initialize the buffer with capacity = SR / frequency. You'll need to
-        //       cast the result of this division operation into an int. For
-        //       better accuracy, use the Math.round() function before casting.
-        //       Your should initially fill your buffer with zeros.
+        // Calculate the capacity (buffer size) based on frequency
+        int capacity = (int) Math.round(SR / frequency);
+
+        // Initialize the buffer with calculated capacity
+        buffer = new ArrayDeque61B<>();
+
+        // Fill the buffer with zeros initially
+        for (int i = 0; i < capacity; i++) {
+            buffer.addLast(0.0);
+        }
     }
 
 
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
-        // TODO: Dequeue everything in buffer, and replace with random numbers
-        //       between -0.5 and 0.5. You can get such a number by using:
-        //       double r = Math.random() - 0.5;
-        //
-        //       Make sure that your random numbers are different from each
-        //       other. This does not mean that you need to check that the numbers
-        //       are different from each other. It means you should repeatedly call
-        //       Math.random() - 0.5 to generate new random numbers for each array index.
+        // Remove all existing elements from the buffer
+        int size = buffer.size();
+        buffer = new ArrayDeque61B<>();
+
+        // Fill with new random numbers between -0.5 and 0.5
+        for (int i = 0; i < size; i++) {
+            double r = Math.random() - 0.5;
+            buffer.addLast(r);
+        }
     }
 
     /* Advance the simulation one time step by performing one iteration of
      * the Karplus-Strong algorithm.
      */
     public void tic() {
-        // TODO: Dequeue the front sample and enqueue a new sample that is
-        //       the average of the two multiplied by the DECAY factor.
-        //       **Do not call StdAudio.play().**
+        // Dequeue the front sample
+        double first = buffer.removeFirst();
+
+        // Get the new front sample (which is now at the front after removal)
+        double second = buffer.get(0);
+
+        // Calculate the new sample: average of the two multiplied by DECAY
+        double newSample = ((first + second) / 2.0) * DECAY;
+
+        // Enqueue the new sample at the back
+        buffer.addLast(newSample);
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
-        // TODO: Return the correct thing.
-        return 0;
+        if (buffer.size() == 0) {
+            return 0.0;
+        }
+        return buffer.get(0);
     }
 }
-    // TODO: Remove all comments that say TODO when you're done.
